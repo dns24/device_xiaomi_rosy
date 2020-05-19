36a11308158f517336fb9b52ed147f4883084e67
Pulkit077 <pulkitagarwal2k1@gmail.com>
2020-05-19 06:56:55 +0530

    rosy: partially revert and rearrange some properties
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- prop.mk
+++ prop.mk
@@ -99,8 +99,7 @@ debug.mdpcomp.logs=0 \
 debug.sf.disable_hwc_vds=1 \
 persist.vendor.dpm.feature=1 \
 ro.hardware.egl=adreno \
-debug.sf.hw=0 \
-debug.sf.latch_unsignaled=0 \
+debug.sf.hw=1 \
 debug.sf.recomputecrop=0 \
 dev.pm.dyn_samplingrate=1 \
 persist.demo.hdmirotationlock=false \
@@ -209,28 +208,22 @@ persist.sys.fflag.override.settings_network_and_internet_v2=true
 
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
 ro.surface_flinger.protected_contents=true \
-ro.surface_flinger.use_smart_90_for_video=true \
-ro.surface_flinger.set_display_power_timer_ms=10000 \
-ro.surface_flinger.set_touch_timer_ms=5000 \
-ro.surface_flinger.set_idle_timer_ms=9000
+ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+ro.surface_flinger.max_virtual_display_dimension=4096 \
+ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
+ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
+ro.surface_flinger.protected_contents=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
-debug.sf.early_phase_offset_ns=1500000 \
-debug.sf.early_app_phase_offset_ns=1500000 \
+debug.sf.early_phase_offset_ns=500000 \
+debug.sf.early_app_phase_offset_ns=500000 \
 debug.sf.early_gl_phase_offset_ns=3000000 \
 debug.sf.early_gl_app_phase_offset_ns=15000000 \
 debug.sf.latch_unsignaled=1 \
 debug.sf.disable_backpressure=1 \
 debug.sf.enable_gl_backpressure=1
-
-# The default sf phase offset is set to 6ms, to avoid it be included into next
-# vsync threshold, set high fps early sf and next vsync threshold phase offset
-# to 6.1ms, which is bigger than all sf phase offsets in normal frame rate.
-PRODUCT_PROPERTY_OVERRIDES += \
-debug.sf.high_fps_early_phase_offset_ns=6100000 \
-debug.sf.high_fps_early_gl_phase_offset_ns=9000000 \
-debug.sf.phase_offset_threshold_for_next_vsync_ns=6100000
 
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
