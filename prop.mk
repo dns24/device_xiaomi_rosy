f6821c6f7955e1778129d6c8bc85e62630356f2a
Pulkit077 <pulkitagarwal2k1@gmail.com>
2020-05-27 14:01:13 +0530

    rosy: switch to userspace LMKD
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- prop.mk
+++ prop.mk
@@ -121,17 +121,13 @@ debug.enable.sglscale=1
 PRODUCT_PROPERTY_OVERRIDES += \
 drm.service.enabled=true
 
-#LKMD
+# LKMD
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.lmk.low=1001 \
-ro.lmk.medium=800 \
-ro.lmk.critical=0 \
-ro.lmk.critical_upgrade=false \
-ro.lmk.upgrade_pressure=100 \
-ro.lmk.downgrade_pressure=100 \
+ro.lmk.use_psi=false \
 ro.lmk.kill_heaviest_task=true \
 ro.lmk.kill_timeout_ms=100 \
-ro.lmk.use_minfree_levels=true
+ro.lmk.use_minfree_levels=true \
+ro.lmk.log_stats=true
 
 # Fingerprint
 PRODUCT_PROPERTY_OVERRIDES += \
