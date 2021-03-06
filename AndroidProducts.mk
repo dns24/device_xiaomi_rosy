bef97c1246e592340ff8a178672d6afbaf9b0d54
Wahid Nursidik <wahidns24@gmail.com>
2021-03-06 08:30:19 +0000

    rosy: bliss bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_rosy.mk
+    $(LOCAL_DIR)/bliss_rosy.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_rosy-userdebug \
-    lineage_rosy-eng
+    bliss_rosy-userdebug \
+    bliss_rosy-eng
